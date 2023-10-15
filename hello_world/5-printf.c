4d05f2e8405eb5797d5678cc50e2448d888f979d
Adnan59250 <adn59250@hotmail.fr>
2023-10-15 04:42:27 -0700

    use the function printf for print a sentence

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - with proper grammar, but the outcome is a piece of art, 
+ *main - "with proper grammar, but the outcome is a piece of art, 
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
